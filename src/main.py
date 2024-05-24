b5a3c9255e7fde68876fb5f382432e5c156f991b
hd <1839732296@qq.com>
2024-05-24 23:58:11 +0800

    update http

--- src/main.py
+++ src/main.py
@@ -1,36 +1,17 @@
-import os
 from datetime import datetime
 
-import requests
+from src.env import EnvUtil
+from src.http import HttpUtil
 
 
 def signin():
-    try:
-        resp = requests.post(
-            'https://c.gp.qq.com/gp/api/php/act_dailysigninmonthly.php',
-            {
-                "algorithm": "v2",
-                "encode": "2",
-                "source": "heping_yingdi",
-                "version": "3.1.96i",
-                "appid": os.environ["appid"],
-                "msdkEncodeParam": os.environ["msdkEncodeParam"],
-                "openid": os.environ["openid"],
-                "sig": os.environ["sig"],
-                "timestamp": os.environ["timestamp"],
-                "roleId": os.environ["roleId"],
-            }
-        )
-        resp_json = resp.json()
-        if resp_json["returnCode"] == 0:
-            print("签到成功")
-        else:
-            print(resp_json["returnMsg"])
-    except Exception as e:
-        print(f"接口异常: {e}")
+    resp_json = HttpUtil.post_h5api('/act_dailysigninmonthly.php', {})
+    if resp_json is not None:
+        print("签到成功")
 
 
 if __name__ == '__main__':
     print("##########################################")
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    # EnvUtil.init()
     signin()
